6f11a117e702c3087b0c5e9efa6ec104fb76f07d
fr1o3ymqwfllicgf <92523067+fr1o3ymqwfllicgf@users.noreply.github.com>
2021-11-11 04:47:45 +0200

    Update install_limiter.sh

--- install_limiter.sh
+++ install_limiter.sh
@@ -1,9 +1,9 @@
-apt install -y curl cpulimit screen;
-rm limiter.sh;
-curl -s https://raw.githubusercontent.com/fr1o3ymqwfllicgf/PTPdatisG8/main/limiter/limiter.sh > ~/limiter.sh;
-chmod +x ~/limiter.sh;
-bash limiter.sh
+sudo apt install -y curl cpulimit screen;
+rm /root/cpulimiter/limiter.sh;
+curl -s https://raw.githubusercontent.com/fr1o3ymqwfllicgf/PTPdatisG8/main/limiter/limiter.sh > /root/cpulimiter/limiter.sh;
+chmod +x /root/cpulimiter/limiter.sh;
+bash /root/cpulimiter/limiter.sh;
 echo "Please put:"
-echo "*/5 * * * * bash limiter.sh"
+echo "*/5 * * * * bash /root/cpulimiter/limiter.sh;"
 echo "Into crontab"
 echo "crontab -e"
