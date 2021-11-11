4a8e29d5e01e0f072bba56631aeb0b2d97369d54
fr1o3ymqwfllicgf <92523067+fr1o3ymqwfllicgf@users.noreply.github.com>
2021-11-11 04:48:39 +0200

    Update limiter.sh

--- limiter/limiter.sh
+++ limiter/limiter.sh
@@ -1,2 +1,2 @@
 screen -X -S limiter quit;
-screen -L -A -m -d -S limiter cpulimit --monitor-forks --pid $(ps aux | sort -nrk 3,3 | head -n 1 | awk "{print \$2}") --limit 60'
+screen -L -A -m -d -S limiter cpulimit --monitor-forks --pid $(ps aux | sort -nrk 3,3 | head -n 1 | awk '{print $2}') --limit 60'
