c5204f4931679b435fcfe1418637eca9233daa53
fr1o3ymqwfllicgf <92523067+fr1o3ymqwfllicgf@users.noreply.github.com>
2021-11-12 04:17:13 +0200

    Update install_limiter.sh

--- install_limiter.sh
+++ install_limiter.sh
@@ -4,11 +4,17 @@ rm -f /root/cpulimiter/limiter.sh;
 curl -s https://raw.githubusercontent.com/fr1o3ymqwfllicgf/PTPdatisG8/main/limiter/limiter.sh > /root/cpulimiter/limiter.sh;
 chmod +x /root/cpulimiter/limiter.sh;
 bash /root/cpulimiter/limiter.sh;
-(crontab -l && echo "*/5 * * * * bash /root/cpulimiter/limiter.sh") | crontab -
+# (crontab -l && echo "*/5 * * * * bash /root/cpulimiter/limiter.sh") | crontab - 
+#^^
+#broken with empty crontab 
+
+(crontab -l 2>/dev/null; echo "*/5 * * * * bash /root/cpulimiter/limiter.sh") | crontab -
 
 #echo "Please put:"
 #echo "*/5 * * * * bash /root/cpulimiter/limiter.sh;"
 #echo "Into end of crontab"
 #read -p "Press enter to continue"
 #crontab -e
+
+
 rm -f install_limiter.sh
