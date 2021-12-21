3829fbd78f8e0fdd649386d9860a1c2c79e9ff5d
fr1o3ymqwfllicgf <92523067+fr1o3ymqwfllicgf@users.noreply.github.com>
2021-12-21 06:40:09 +0200

    Update update_wal_aleo2.sh

--- update_wal_aleo2.sh
+++ update_wal_aleo2.sh
@@ -13,4 +13,4 @@ WantedBy=multi-user.target
 " > /etc/systemd/system/aleod-miner.service
 systemctl daemon-reload
 service aleod-miner restart
-echo "New Aleo Address (old) - $(cat /etc/systemd/system/aleod-miner.service | awk '/ --trial --miner / {print $4}' | tail -1)
+echo "New Aleo Address (old) - $(cat /etc/systemd/system/aleod-miner.service | awk '/ --trial --miner / {print $4}' | tail -1)"
