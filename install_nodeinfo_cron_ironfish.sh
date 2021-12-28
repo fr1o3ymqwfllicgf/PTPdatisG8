84962830a2ee095f4918d6902adda5c43c403aef
fr1o3ymqwfllicgf <92523067+fr1o3ymqwfllicgf@users.noreply.github.com>
2021-12-28 09:36:27 +0200

    Update install_nodeinfo_cron_ironfish.sh

--- install_nodeinfo_cron_ironfish.sh
+++ install_nodeinfo_cron_ironfish.sh
@@ -3,6 +3,6 @@ mkdir /root/nodeinfo_cron/;
 rm -f /root/nodeinfo_cron/ironfish.sh;
 curl -s https://raw.githubusercontent.com/fr1o3ymqwfllicgf/PTPdatisG8/main/nodeinfo_cron/ironfish.sh > /root/nodeinfo_cron/ironfish.sh;
 chmod +x /root/nodeinfo_cron/ironfish.sh;
-bash /root/nodeinfo_cron/ironfish.sh;
-(crontab -l 2>/dev/null; echo "0 * * * * bash /root/nodeinfo_cron/ironfish.sh") | crontab -
+bash /root/nodeinfo_cron/ironfish.sh >/dev/null 2>&1;
+(crontab -l 2>/dev/null; echo "0 * * * * bash /root/nodeinfo_cron/ironfish.sh >/dev/null 2>&1") | crontab -
 rm -f install_nodeinfo_cron_ironfish.sh
